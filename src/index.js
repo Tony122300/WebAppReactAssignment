a500f04da9f578eb0b6ee09b9aa2b7271f3da361
Tony JinHuiLiu <20090185@mail.wit.ie>
2022-11-30 19:24:39 +0000

    Bug Fix - make action icons on movie card configurable.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-//import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader'
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
-
+import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
 const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -25,13 +26,16 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
         <BrowserRouter>
             <SiteHeader />
+            <MoviesContextProvider>
         <Routes>
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
           <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
         </Routes>
+        </MoviesContextProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
